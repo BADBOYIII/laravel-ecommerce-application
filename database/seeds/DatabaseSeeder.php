ad619ba366138738aacb2b1d41e49ddcced99335
BADBOYIII <apisit_xyz2000@yahoo.com>
2020-04-16 23:56:28 +0700

    categories model and migrations added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([AdminsTableSeeder::class, SettingsTableSeeder::class]);
+        $this->call([
+            AdminsTableSeeder::class,
+            SettingsTableSeeder::class,
+            CategoriesTableSeeder::class,
+        ]);
     }
 }
